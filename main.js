93e5b184b095ab591f8bb97d174a7c4397a9379b
Lutaaya Jamil <kunoomaking@gmail.com>
2023-04-17 20:38:43 +0300

    settingUp middlewares

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 //imports
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 //database connection
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to the database!"));
 
+//middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "my secret key",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+
+//set template engine
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-    res.send("Hello world");
+  res.send("Hello world");
 });
 
 app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`)
-})
+  console.log(`server started at http://localhost:${PORT}`);
+});
