1df6892ed125a36f9b4bb090cd712465eb69aaf6
Lutaaya Jamil <kunoomaking@gmail.com>
2023-04-18 02:12:12 +0300

    all routes set

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,20 @@ router.get('/', (req, res) => {
     res.render('index', {title: 'Home Page'});
 });
 
+router.get('/add_request', (req, res) => {
+    res.render('add_request', {title: 'add request'});
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', {title: 'Login'});
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dash', {title: 'DashBoard'});
+});
+
+router.get('/view', (req, res) => {
+    res.render('view', {title: 'View'});
+});
+
 module.exports = router;
