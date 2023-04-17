2472e57c9128f1d0aeeaef909a8bc912f72ace61
Lutaaya Jamil <kunoomaking@gmail.com>
2023-04-17 20:25:56 +0300

    connecting to mongodb

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//database connection
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to the database!"));
+
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
