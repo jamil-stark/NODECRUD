5808a91eb39ee0e213761a6c83989046c7bf3f93
Lutaaya Jamil <kunoomaking@gmail.com>
2023-04-18 07:24:35 +0300

    delete works

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../models/messages');
 const multer = require('multer');
+const fs = require('fs');
 
 //image uplaod 
 var storage = multer.diskStorage({
@@ -37,6 +38,8 @@ router.post('/add_request', uplaod, (req, res) => {
             res.json({message: err.message, type: 'danger'});
         });
 });
+
+
 
 
 router.get('/', (req, res) => {
@@ -87,5 +90,62 @@ router.get('/dashboard', (req, res) => {
 router.get('/view', (req, res) => {
     res.render('view', {title: 'View'});
 });
+
+//update user
+router.post('/update/:id', uplaod, (req, res) => {
+    let id = req.params.id;
+    let new_image = '';
+
+    if(req.file){
+        new_image = req.file.filename;
+        try{
+            fs.unlinkSync("./uploads/" + req.body.old_image);
+        }catch(err){
+            console.log(err);
+        }
+    } else {
+        new_image = req.body.old_image;
+    }
+
+    Message.findByIdAndUpdate(id, {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        image: new_image,
+    })
+    .then(result => {
+        req.session.message = {
+            type: 'success',
+            message: 'User updated successfully',
+        };
+        res.redirect('/')
+    })
+    .catch(err => {
+        res.json({message: err.message, type: 'danger'});
+    })
+});
+
+//delete user route
+router.get('/delete/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await Message.findByIdAndRemove(id);
+      if (result.image !== '') {
+        try {
+          fs.unlinkSync('./uploads/' + result.image);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      req.session.message = {
+        type: 'info',
+        message: 'Request deleted!'
+      };
+      res.redirect('/');
+    } catch (err) {
+      res.json({ message: err.message });
+    }
+  });
+    
 
 module.exports = router;
